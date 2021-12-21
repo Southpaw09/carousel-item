091a304c97b5cd780c339be5e25f723bfaa7adcc
Ayush <86900579+Southpaw09@users.noreply.github.com>
2021-12-21 20:18:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let currentSlide = 1;
+
+
+function showSlide(slideIndex) {
+  const slides = document.getElementsByClassName('carouselImgs');
+  if (slideIndex > slides.length) { currentSlide = 1 }
+  if (slideIndex < 1) { currentSlide = slides.length }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[currentSlide - 1].style.display = 'block';
+}
+
+
+function nextSlide() {
+  showSlide(currentSlide += 1);
+}
+
+function previousSlide() {
+  showSlide(currentSlide -= 1);
+}
+
+window.onload = function () {
+  showSlide(currentSlide);
+  document.getElementById('prev').addEventListener('click', function () {
+    previousSlide();
+  })
+  document.getElementById('next').addEventListener('click', function () {
+    nextSlide();
+  })
+}
